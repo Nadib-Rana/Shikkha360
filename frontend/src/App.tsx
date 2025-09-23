e688f08ad16f0856981cdcf0d8da3a09531668c9
Nadib Rana <codecrafersnadib@gmail.com>
2025-09-23 21:05:30 +0600

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,9 @@
-<<<<<<< HEAD
-import React from 'react'
-import ApplicationFrom from './components/applicationFrom'
-import Switch from './components/switch'
-
 const App = () => {
   return (
     <div>
-      <Switch />
+      Hello Nadib.
     </div>
   )
 }
 
 export default App
-=======
-import './App.css';
-import Login from './pages/auth/Login';
-import SignUp from './pages/auth/Register';
-
-function App() {
-  return (
-    <>
-      <Login />
-      <SignUp />
-    </>
-  );
-}
-
-export default App;
->>>>>>> 19161e85290298bacfe8408fe5cfc081895564fa
